e31f363091c59fd204c6c87b2b373bed9e920eaf
zgz <1587688408@qq.com>
2021-09-29 16:23:35 +0800

    Utility Types

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -43,10 +43,61 @@ export const http = async (endpoint: string, {data, token, headers, ...customCon
         })
 }
 
+// js 的 typeof 是在ruantime的时候运行的。
+// return typeof 1 === 'number
+
+
 export const useHttp = () => {
     const {user} = useAuth()
     // return ([endpoint, config] : [string, Config]) => http(endpoint, {...config, token: user?.token})
 
-    // TODO TS操作符
-    return (...[endpoint, config] : Parameters<typeof http>) => http(endpoint, {...config, token: user?.token})
+    // TS 中的 typeof 是在静态环境运行的
+    // TODO  TS Utility Types
+    // Utility Types 的用法： 用泛型传入其他类型， 用Utility type对这个类型进行某种操作
+    return (...[endpoint, config]: Parameters<typeof http>) => http(endpoint, {...config, token: user?.token})
 }
+
+// 联合类型
+let myFavoriteNumber: string | number
+myFavoriteNumber = 'seven'
+myFavoriteNumber = 123131
+
+let jackFavoriteNumber
+
+// 类型别名  这种情况下 interface没法替代type
+type FavoriteNumber = string | number
+let roseFavoriteNumber: FavoriteNumber = '6'
+
+// interface 和 类型别名 区别 定义联合类型 交叉类型
+
+// interface 没法实现 Utility type
+
+type Person = {
+    name: string,
+    age: number
+}
+
+// const xiaoMing: Person = { name: 'xiaoming' }
+const xiaoMing: Partial<Person> = {
+    name: 'xiaoMing'
+}
+
+// 有 age 没 name
+const shenmi1: Omit<Person, 'name'> = {
+    age: 11
+}
+
+const shenmi2: Omit<Person, 'name' | 'age'> = {
+    age: 11
+}
+
+// Partial 的实现
+type Partial<T> = {
+    [P in keyof T]?: T[P]
+};
+
+type Pick<T, K extends keyof T> = {
+    [P in K]: T[P];
+};
+
+type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
